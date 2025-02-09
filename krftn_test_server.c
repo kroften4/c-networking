982877c0b08c02e2e8b94e47cf7dfd835eb93b81
kroften <sskozhemyakin2006@gmail.com>
2025-02-09 14:42:03 +0300

    Choose next addrinfo if first fails; accept indefinetely

--- krftn_test_server.c
+++ krftn_test_server.c
@@ -5,80 +5,130 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define SERVER_PORT "3490"
 #define BACKLOG 10
 
+void *get_in_addr(struct sockaddr_storage *ss)
+{
+    if (ss->ss_family == AF_INET) {
+        return &(((struct sockaddr_in *)ss)->sin_addr);
+    }
+    return &(((struct sockaddr_in6 *)ss)->sin6_addr);
+}
+
 int main(int argc, char *argv[]) {
-    int errnosave = 0, status, sockfd, connfd;
+    int errno_save = 0, status, sockfd, connfd;
     struct addrinfo hints;
     struct addrinfo *res;
 
+    if (argc != 2) {
+        printf("usage: krftn_test_server <port>\n");
+        return EXIT_FAILURE;
+    }
+
+    char *server_port = argv[1];
+
     // get addrinfo for host
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
+    hints.ai_family = AF_INET; // ipv4 (TODO: make switches to choose)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_addr = (struct sockaddr *)AI_PASSIVE;
-    if ((status = getaddrinfo(NULL, SERVER_PORT, &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    if ((status = getaddrinfo(NULL, server_port, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         return EXIT_FAILURE;
     }
 
-    // select the appropriate addrinfo linked list member
-    if (res == NULL) {
-        perror("couldn't find host addrinfo (list is empty)");
+    // create a socket and bind
+    struct addrinfo *p;
+    for (p = res; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            errno_save = errno;
+            fprintf(stderr, "socket: %s\n", strerror(errno_save));
+            continue;
+        }
+        
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            errno_save = errno;
+            close(sockfd);
+            fprintf(stderr, "bind: %s\n", strerror(errno_save));
+            continue;
+        }
+
+        break; // successfull socket() and bind()
+    }
+
+    if (p == NULL) {
+        perror("server: can't create a socket");
         return EXIT_FAILURE;
     }
-    struct addrinfo server_ai = *res;
-    freeaddrinfo(res);
 
-    // make a socket, bind, and listen
-    if ((sockfd = socket(server_ai.ai_family, server_ai.ai_socktype, server_ai.ai_protocol)) == -1) {
-        errnosave = errno;
-        perror("failed to create a socket");
-        goto cleanup;
-    }
-    
-    if (bind(sockfd, server_ai.ai_addr, server_ai.ai_addrlen) == -1) {
-        errnosave = errno;
-        perror("failed to bind socket");
-        goto cleanup;
-    }
+    struct addrinfo server_ai = *p;
+    freeaddrinfo(p);
 
     if (listen(sockfd, BACKLOG) == -1) {
-        errnosave = errno;
-        perror("failed to listen on socket FD");
-        goto cleanup;
+        errno_save = errno;
+        close(sockfd);
+        perror("listen");
+        return errno_save;
     }
+
     char server_address[INET6_ADDRSTRLEN];
-    struct sockaddr_in *sa_in = (struct sockaddr_in *)server_ai.ai_addr;
-    inet_ntop(server_ai.ai_family, &sa_in->sin_addr, server_address, server_ai.ai_addrlen);
-    printf("listening on %s:%s\n", server_address, SERVER_PORT);
-
-    struct sockaddr_storage conn_addr;
-    socklen_t conn_addrsize;
-    if ((connfd = accept(sockfd, (struct sockaddr *) &conn_addr, &conn_addrsize)) == -1) {
-        errnosave = errno;
-        perror("failed to accept connection");
-        goto cleanup;
+    inet_ntop(server_ai.ai_family, get_in_addr((struct sockaddr_storage *)server_ai.ai_addr), server_address, server_ai.ai_addrlen);
+    if (server_ai.ai_family == AF_INET) {
+        printf("listening on %s:%s\n", server_address, server_port);
     }
-    printf("some individual has connected\n");
-
-    char test_msg[] = "Hello C Networking!";
-    ssize_t test_msglen = sizeof test_msg;
-    printf("test message: %s\ntest message size: %zu", test_msg, test_msglen);
-    ssize_t size_sent = send(connfd, test_msg, test_msglen, 0);
-    if (size_sent == -1) {
-        errnosave = errno;
-        perror("fail in send()");
-        goto cleanup;
+    if (server_ai.ai_family == AF_INET6) {
+        printf("listening on [%s]:%s\n", server_address, server_port);
     }
-    printf("sent %zu/%zu of data to some individual\n", size_sent, test_msglen);
 
-    cleanup:
-    close(connfd);
+    // accept connections
+    for (;;) {
+        struct sockaddr_storage conn_addr;
+        socklen_t conn_addrsize = sizeof conn_addr;
+        if ((connfd = accept(sockfd, (struct sockaddr *) &conn_addr, &conn_addrsize)) == -1) {
+            errno_save = errno;
+            perror("accept");
+            goto conn_cleanup;
+        }
+
+        int pid = fork();
+        if (pid == -1) {
+            errno_save = errno;
+            perror("fork");
+            goto conn_cleanup;
+        }
+        if (!pid) {
+            close(sockfd);
+            printf("some individual has connected\n");
+            char test_msg[] = 
+                "HTTP/1.0 200 OK\n"
+                "Content-Type: text/html\n"
+                "Content-Length: 105\n"
+                "\n"
+                "<!DOCTYPE html><html><head><title>C HTTP server</title></head><body>I am the one who serves</body></html>"
+            ;
+            ssize_t test_msglen = sizeof test_msg;
+            ssize_t size_sent = send(connfd, test_msg, test_msglen, 0);
+            if (size_sent == -1) {
+                errno_save = errno;
+                fprintf(stderr, "send: %s\n", strerror(errno_save));
+                exit(errno_save);
+            }
+            printf("sent %zu/%zu bytes of data to some individual\n", size_sent, test_msglen);
+            exit(0);
+        }
+        wait(NULL);
+
+        conn_cleanup:
+        close(connfd);
+        if (errno_save != 0) {
+            fprintf(stderr, "%s\n", strerror(errno_save));
+        }
+    }
+
     close(sockfd);
-    return errnosave;
+    return errno_save;
 }
